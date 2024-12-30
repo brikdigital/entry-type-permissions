7fa06d6324b1177aec02e7fc2d50fed5d56b2b89
Alyxia Sother <alyxia@riseup.net>
2024-12-30 15:41:31 +0100

    Make it work & update permission generation

--- src/EntryTypePermissions.php
+++ src/EntryTypePermissions.php
@@ -5,7 +5,12 @@ namespace brikdigital\entrytypepermissions;
 use Craft;
 use craft\base\Event;
 use craft\base\Plugin;
+use craft\elements\Entry;
+use craft\events\DefineEntryTypesEvent;
 use craft\events\RegisterUserPermissionsEvent;
+use craft\fields\Matrix;
+use craft\models\EntryType;
+use craft\models\Section;
 use craft\services\UserPermissions;
 
 class EntryTypePermissions extends Plugin
@@ -21,33 +26,60 @@ class EntryTypePermissions extends Plugin
 
         if (Craft::$app->getRequest()->getIsCpRequest()) {
             $this->registerUserPermissions();
+            $this->registerEntryTypeFilter();
         }
+    }
+
+    public function registerEntryTypeFilter(): void
+    {
+        Event::on(Entry::class, Entry::EVENT_DEFINE_ENTRY_TYPES, function (DefineEntryTypesEvent $event) {
+            $event->entryTypes = $this->_filterEntryTypes($event->entryTypes);
+        });
+
+        Event::on(Matrix::class, Matrix::EVENT_DEFINE_ENTRY_TYPES, function (DefineEntryTypesEvent $event) {
+            $event->entryTypes = $this->_filterEntryTypes($event->entryTypes);
+        });
     }
 
     public function registerUserPermissions(): void
     {
         Event::on(UserPermissions::class, UserPermissions::EVENT_REGISTER_PERMISSIONS,
             function (RegisterUserPermissionsEvent $event) {
-                $sections = Craft::$app->entries->getAllSections();
-
-                $sectionEntryTypes = [];
-                foreach ($sections as $section) {
-                    $sectionEntryTypes = array_merge($sectionEntryTypes, $section->entryTypes);
-                }
-                $sectionEntryTypes = array_values(array_unique($sectionEntryTypes));
+                $types = Craft::$app->entries->getAllEntryTypes();
+                $types = array_filter($types, function (EntryType $type) {
+                    $usages = array_map(get_class(...), $type->findUsages());
+                    $usages = array_filter($usages, function (string $type) {
+                        return match($type) {
+                            Matrix::class => true,
+                            Section::class => true,
+                            default => false,
+                        };
+                    });
+                    return count($usages) !== 0;
+                });
 
                 $perms = [];
                 array_map(function ($entryType) use (&$perms) {
                     $perms["createEntryType:$entryType->uid"] = [
                         "label" => $entryType->name,
                     ];
-                }, $sectionEntryTypes);
+                }, $types);
 
                 $event->permissions[] = [
                     'heading' => 'Entry Type Permissions',
                     'permissions' => $perms
                 ];
             }
+        );
+    }
+
+    private function _filterEntryTypes(array $entryTypes): array
+    {
+        return array_values( // reindex the array
+            // filter out any entry types we don't have access to
+            array_filter($entryTypes, function (EntryType $type) {
+                return Craft::$app->user->checkPermission("createEntryType:$type->uid");
+            })
         );
     }
 }
